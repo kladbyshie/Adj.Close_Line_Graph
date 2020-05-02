25cd11d2e2ac791c5af7b56c6f252ce0b8d1d088
ngaragulya <ngaragulya@gmail.com>
2020-05-02 13:52:07 -0400

    v0.01

--- FlaskAdj.Close/app/routes.py
+++ FlaskAdj.Close/app/routes.py
@@ -5,10 +5,12 @@ from flask_wtf import FlaskForm
 from wtforms.fields.html5 import DateField
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from waitress import serve
 
 app = Flask(__name__)
-#In an app hosted on a site, this would be in a separate config file, and likely generated for each website instance.
-app.config['SECRET_KEY'] = 'h1j3ch184yv12893ujcop2ije92p18h4v1892j'
+#rudimentary security key
+app.config['SECRET_KEY'] = '2e828bc1828af6a59a88af8247f13519a9f95e4c033fb603fecf5c483356915c621fe5565e7b95f53b0b2e28144d326a30283dabfd4999e071918edc6056372d'
+
 
 #The form used within the requests, describing all the fields and including an input validator for the dates.
 #The default dates are 1 year ago from today to today, and default stocks are FAANG.
@@ -16,7 +18,7 @@ class GraphForm(FlaskForm):
     yearago = date.today() - timedelta(days=365)
     startdate = DateField('Start Date',[DataRequired()], default=yearago)
     enddate = DateField('End Date',[DataRequired()], default=date.today)
-    stocklist = StringField('Stocks', [DataRequired()], default="FB, AAPL, AMZN, NFLX, GOOG")
+    stocklist = StringField('Stock tickers', [DataRequired()], default="FB, AAPL, AMZN, NFLX, GOOG")
     submit = SubmitField('Submit')
 
     def validate_on_submit(self):
@@ -32,13 +34,10 @@ def main():
     form = GraphForm()
     message = None
     if form.validate_on_submit():
-        stocklist=cleaner(form.stocklist.data.split(","))
-        val = grapher(form.startdate.data, form.enddate.data, stocklist)
-        script = val[0]
-        div1 = val[1]
-        cdn_js = val[2]
-        return render_template('main.html', script=script, div1=div1, cdn_js=cdn_js, form=form)
+        stockdata=cleaner(form.stocklist.data.split(","))
+        htmlcode = grapher(stockdata, form.startdate.data, form.enddate.data)
+        return render_template('main.html', form=form, htmlcode=htmlcode)
     return render_template('main.html', form=form, message=message)
                 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    serve(app, host='127.0.0.1', port=5000)
